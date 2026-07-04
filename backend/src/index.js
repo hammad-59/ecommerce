083cac03325bbeb0242dc5d0d7c3b59f48f9dc3b
hammad-59 <muhdhammad59@gmail.com>
2026-07-04 19:26:53 +0500

    remove dns settings

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from "./app.js"
-import dns from "dns"
-dns.setServers(["1.1.1.1", "8.8.8.8"])
 
 dotenv.config({
     path: './.env'
